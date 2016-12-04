7615df5288045a2e9cf949ca35a7e1c7cb23c1d1
Sam Kilgus <samkilgus@gmail.com>
2016-12-04 17:53:03 -0500

    switched to firebase

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ var center;
 var defalut_style = [{"elementType":"geometry","stylers":[{"color":"#1c1c1c"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#212121"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#757575"},{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#181818"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"poi.park","elementType":"labels.text.stroke","stylers":[{"color":"#1b1b1b"}]},{"featureType":"road","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#2c2c2c"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#8a8a8a"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#373737"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#3c3c3c"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"water","stylers":[{"color":"#797979"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#3a3a3a"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#3d3d3d"}]}];
 var my_id;
 var locs = {
-  me: {lat:43.0861,lng:-77.6705},
+  me: {lat:43.08,lng:-77.67},
 };
 var overlay_hover = false;
 
@@ -18,16 +18,16 @@ function r_in_r(min, max) {
 
 function initMap() {
   setTimeout(function () {
-    console.log("asdf");
     render_map()
   }, 1000);
 }
 
 function render_map() {
+
   // Styles a map in night mode.
   try {
     navigator.geolocation.getCurrentPosition(function(location) {
-      locs.you = {lat: location.coords.latitude,lng: location.coords.longitude};
+      locs.you = {lat: parseFloat((location.coords.latitude).toFixed(2)),lng: parseFloat((location.coords.longitude).toFixed(2))};
       center = locs.you;
       map = new google.maps.Map(document.getElementById('map'), {
         center: center,
@@ -82,7 +82,6 @@ function render_map() {
 }
 
 function update_locs() {
-  console.log(locs);
   var me_circle = new google.maps.Circle({
     strokeColor: "#ffcc00",
     strokeOpacity: 0.8,
@@ -112,7 +111,7 @@ function go_to_link(element) {
     'linkedin': 'https://www.linkedin.com/in/samkilgus',
     'github': 'https://github.com/returningsam',
     'instagram': 'https://www.instagram.com/returningsam/',
-    'resume': window.location.href + 'get_resume'
+    'resume': 'resume.pdf'
   }
   window.open(links[element.innerHTML],'_blank');
 }
@@ -211,7 +210,6 @@ function show_page() {
 }
 
 window.onload = function () {
-  document.getElementById('logo').src = window.location.href + "get_logo";
   document.getElementById('overlay').style.borderBottom = (window.innerWidth*4).toString() + "px solid white";
   document.getElementById('overlay').style.borderLeft = (window.innerHeight*4).toString() + "px solid transparent";;
 }
