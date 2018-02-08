24e930d64f9963a0cc411db0840c3416dfbe8e71
Sam Kilgus <samkilgus@gmail.com>
2018-02-08 13:25:06 -0500

    updated details

--- public/playground/main.js
+++ public/playground/main.js
@@ -100,7 +100,7 @@ var pgData = {
         description: "Draws a ungulating blob around the mouse."
     },
     "doodle_bot": {
-        date: "07.02.2018",
+        date: "06.02.2018",
         tags: ["interactive","canvas","lines"],
         mobile: true,
         description: "Draws really long random line while (optionally) filling in some parts to create a random doodle."
